4a71250d1617d98ec01c4a68e2d8b67e04be701d
Rafael Paz <99267530+rafahpaz@users.noreply.github.com>
2022-05-17 12:38:28 -0300

    Add files via upload

---
+++ Exercicios aula 9/atv1.js
@@ -0,0 +1,5 @@
+Nomes = ['Rafael', 'Davi', 'Pedro', 'Matheus', 'Bernardo', 'Alex']
+
+for ( let i = 0; i < Nomes.length; i++){
+    console.log(Nomes[i])
+}
---
+++ Exercicios aula 9/atv4.js
@@ -0,0 +1,5 @@
+Salarios = ['3200$', '2500$', '1450$', '2100$']
+
+for ( let i = 0; i < Salarios.length; i++){
+    console.log(Salarios[i])
+}
